4aac33dab6e86a06b5b093eeb6a612a58f363f63
HackThePlanet <matt.h1@gmx.at>
2012-09-25 21:08:38 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'a220623040099aaf3c7ba3b6182829e1460aebc4bb404259ae521c822e5f570c2a973e4161d9433ca8f95d8edd9e437a439890d48d25d374f891225c45bea17a'
